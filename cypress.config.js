97ca160645c43fd31da5364fc9264809fbc46479
Alexander Pajer <alexander.pajer@integrations.at>
2022-11-30 22:54:54 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
   },
   video: false,
   e2e: {
-    baseUrl: 'http://food-shop-ui-e2e-tests.westeurope.azurecontainer.io/',
+    baseUrl: 'http://food-shop-ui.westeurope.azurecontainer.io/',
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
